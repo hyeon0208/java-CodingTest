d148b65b14241c00ce7b362ad80d462fb11472bb
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-10-10 23:56:10 +0900

    Lv1 : 자릿수더하기- while문 사용

--- lv1/자릿수더하기2.java
+++ lv1/자릿수더하기2.java
@@ -6,7 +6,9 @@ public class Solution {
         int answer = 0;
 
         while (n != 0) {
+            // 입력한 값을 10으로 나눈 나머지를 answer에 더해준다.
             answer += n % 10;
+            // 입력한 값을 10으로 나눈 몫을 n에 저장한다.
             n /= 10;
         }
 
