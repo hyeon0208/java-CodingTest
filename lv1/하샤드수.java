425cecc6c131e5219589852e6175824e3091e430
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-10-12 00:11:49 +0900

    하샤드수

---
+++ lv1/하샤드수.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean solution(int x) {
+        int sum = 0;
+        int n = x;
+        
+        while(n != 0) {
+            sum += n % 10;
+            n /= 10;
+        }
+        
+        return x % sum == 0 ? true : false;
+    }
+}
