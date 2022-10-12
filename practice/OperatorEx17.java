0a436f10d53cd69e9d8232696486cab3ea85f978
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-10-12 18:43:36 +0900

    Math.round 메서드로 pi 반올림

---
+++ practice/OperatorEx17.java
@@ -0,0 +1,15 @@
+public class OperatorEx17 {
+    public static void main(String[] args) {
+		double pi = 3.141592;
+		double shortPi = (int)(pi * 1000 + 0.5) / 1000.0;
+		
+		System.out.println("pi는 :" + shortPi);
+		
+		double pi2 = 3.141592;
+		// Math.rounts는 소숫점 첫째자리를 반올림한다.
+		double shortPi2 = Math.round(pi2 * 1000)/ 1000.0;
+		
+		System.out.println("pi2는 : " + shortPi2);
+	}
+
+}
