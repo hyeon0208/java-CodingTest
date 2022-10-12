de68ffa9e4a56fef1a21a82df8135d17ae346812
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-10-13 00:06:25 +0900

    Lv1: 정수내림차순으로 배치하기

---
+++ lv1/정수내림차순.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+	public long solution(long n) {
+		String[] arr = String.valueOf(n).split("");
+		Arrays.sort(arr); // 오름차순으로 sort
+
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < arr.length; i++) {
+			sb.append(arr[i]); //내림차순으로 더하기
+		}
+        
+		return Long.parseLong(sb.reverse().toString());
+
+	}
+}
