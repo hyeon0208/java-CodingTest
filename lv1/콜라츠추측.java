81f08be585b429b12efa26073f3690323c424a81
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-10-13 23:23:18 +0900

    Lv1: 콜라츠 추측 - long 사용 이유

--- lv1/콜라츠추측.java
+++ lv1/콜라츠추측.java
@@ -1,6 +1,6 @@
 class Solution {
     public int solution(int num) {
-        long n = num; // int 로 할 시 오버플로우 발생
+        long n = num; // int 로 할 시 짝수, 홀수를 반복하다 표현가능한 범위인 21억을 넘어가 오버플로우 발생
         int answer = 0;
         
         // n = 1 이거나 500번 반복하면 종료
