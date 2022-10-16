9188be60e641862c5ad9cace9af99addd96ec04c
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-10-16 16:41:22 +0900

    Lv1: 핸드폰 번호 가리기

---
+++ lv1/번호가리기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String solution(String phone_number) {
+        char[] chArr = phone_number.toCharArray();
+        for (int i = 0; i < chArr.length-4; i++ ) {
+            chArr[i] = '*';
+        }
+        
+        return String.valueOf(chArr);
+    }
+}
