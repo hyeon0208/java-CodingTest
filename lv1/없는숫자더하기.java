e21a4debcc44195ecd62527642ecd5c81a74c505
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-10-17 23:16:37 +0900

    Lv1: 없는 숫자 더하기

---
+++ lv1/없는숫자더하기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = 45;
+        
+        for (int i : numbers) {
+                answer -= i;
+            }
+        
+        
+        return answer;
+    }
+}
