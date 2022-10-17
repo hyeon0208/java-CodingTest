1b9ac0c025f51a2036db7f5a7a69562c6faf2171
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-10-17 20:55:32 +0900

    참조형 매개변수 테스트 : static, void 설명 추가

--- practice/ReferenceParamEx.java
+++ practice/ReferenceParamEx.java
@@ -13,6 +13,7 @@ public class ReferenceParamEx {
 
 	}
 
+    	// static을 붙이면 객체 생성 생략 가능, void는 return 생략 가능
 	static void change(Data d) { // 참조형 매개변수 (Data 클래스를 참조)
 		d.x = 1000;
 		System.out.println("change() : x = " + d.x); // d 객체의 x 값을 1000으로 변경
