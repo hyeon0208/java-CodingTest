73cf81e33c393714bbf6f732cde5e0fb152ee2ce
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-10-21 16:53:33 +0900

    참조변수와 인스턴스의 연결 Test 2

---
+++ practice/다형성/BindingTest2.java
@@ -0,0 +1,24 @@
+class BindingTest2 {
+	public static void main(String[] args) {
+		Parent p = new Child();
+		Child c = new Child();
+
+		System.out.println("p.x = " + p.x); 
+		p.method();
+
+		System.out.println("c.x = " + c.x);
+		c.method();
+	}
+}
+
+class Parent {
+	int x = 100;
+
+	void method() {
+		System.out.println("Parent Method");
+	}
+}
+
+
+// Child 클래스에는 아무런 멤버도 정의되어 있지 않아, 상속받은 멤버와 메서드만을 가진다.
+class Child extends Parent { }
