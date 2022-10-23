84889833857c0e5e6949e5494442b4ea04c5a94f
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-10-23 23:25:53 +0900

    Lv1: 부족한 금액 계산하기

---
+++ lv1/부족한금액계산.java
@@ -0,0 +1,12 @@
+class Solution {
+    public long solution(int price, int money, int count) {
+        long answer = 0;
+        long sum = 0;
+        for(int i = 1; i <= count; i++) {
+            sum += price * i; 
+        }
+        answer = money - sum;
+        
+        return answer > 0 ? 0 : (long)Math.abs(answer);
+    }
+}
