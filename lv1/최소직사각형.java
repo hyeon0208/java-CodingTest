d93d76f7ddb73e28f16a16cd7cbdbb8fc53f3846
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-11-07 23:23:36 +0900

    feat : 다른 풀이의 최적의 명함지갑 만들기 코드 추가

--- lv1/최소직사각형.java
+++ lv1/최소직사각형.java
@@ -13,3 +13,26 @@ class Solution {
         return maxWidth * maxHeigth;
     }
 }
+
+/*
+  class Solution {
+    public int solution(int[][] sizes) {
+        int maxWidth = 0;
+        int maxHeigth = 0;
+
+        for (int i = 0; i < sizes.length; i++) {
+            int width = Math.max(sizes[i][0], sizes[i][1]);
+            int heigth = 0;
+            if (sizes[i][0] < sizes[i][1]) {
+                heigth = sizes[i][0];
+            } else {
+                heigth = sizes[i][1];
+            }
+
+            maxWidth = Math.max(maxWidth, width);
+            maxHeigth = Math.max(maxHeigth, heigth);
+        }
+        return maxWidth * maxHeigth;
+    }
+}
+*/
