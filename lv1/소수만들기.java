b201ea7631c6927f07f563458ba76f72470965e3
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-11-25 11:46:58 +0900

    refactor : 주어진 숫자 3개의 수를 더해 소수가 되는 경우의 수 구하기

---
+++ lv1/소수만들기.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int solution(int[] nums) {
+        int answer = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                for (int k = j + 1; k < nums.length; k++) {
+                    int num = nums[i] + nums[j] + nums[k];
+                    if (checkPrime(num)) {
+                        answer++;
+                    }
+                }
+            }
+        }
+        return answer;
+    }
+
+    private static boolean checkPrime(int num) {
+        boolean check = true;
+        if (num == 2) {
+            check = true;
+        }
+        for (int i = 2; i < num; i++) {
+            if (num % i == 0) {
+                check = false;
+                break;
+            }
+        }
+        return check;
+    }
+}
