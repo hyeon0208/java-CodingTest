e81eccd2457141fe26065091b191ade898b4be08
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-11-27 01:17:54 +0900

    refactor : 변수의 의미 부여

--- lv1/실패율.java
+++ lv1/실패율.java
@@ -2,22 +2,22 @@ import java.util.*;
 
 class Solution {
     public static int[] solution(int N, int[] stages) {
-        int[] failUsers = new int[N];
+        int[] failCount = new int[N];
         Map<Integer, Double> rate = new LinkedHashMap<>();
         Arrays.sort(stages);
 
         for (int i = 1; i <= N; i++) {
             for (int j = 0; j < stages.length; j++) {
                 if (stages[j] == i) {
-                    failUsers[i-1] += 1;
+                    failCount[i-1] += 1;
                 }
             }
         }
 
         int denominator = stages.length;
-        for (int i = 0; i < failUsers.length; i++) {
+        for (int i = 0; i < failCount.length; i++) {
             double failRate = 0;
-            int numerator = failUsers[i];
+            int numerator = failCount[i];
             if (denominator == 0) {
                 rate.put(i + 1, (double) 0);
                 continue;
@@ -33,9 +33,9 @@ class Solution {
             denominator -= numerator;
         }
 
-        List<Integer> test = new ArrayList<>(rate.keySet());
-        Collections.sort(test, (o1, o2) -> Double.compare(rate.get(o2), rate.get(o1)));
+        List<Integer> failRates = new ArrayList<>(rate.keySet());
+        Collections.sort(failRates, (o1, o2) -> Double.compare(rate.get(o2), rate.get(o1)));
 
-        return test.stream().mapToInt(Integer::intValue).toArray();
+        return failRates.stream().mapToInt(Integer::intValue).toArray();
     }
 }
