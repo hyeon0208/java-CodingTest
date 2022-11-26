ecc986b5a56c0b9c7c08184fff13087d739edcb2
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-11-26 22:58:22 +0900

    refactor : 간략하게 코드 수정

--- lv1/콜라문제.java
+++ lv1/콜라문제.java
@@ -1,13 +1,10 @@
 class Solution {
     public int solution(int a, int b, int n) {
         int answer = 0;
-        while (true) {
-            if (n < a) {
-                break;
-            }
-            int bottle = (n / a) * b;
+        while (!(n < a)) {
+            int bottle = (n/a) * b;
             answer += bottle;
-            n = bottle + n%a;
+            n = bottle + (n%a);
         }
         return answer;
     }
