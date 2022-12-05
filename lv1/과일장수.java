50938fa93966a0103e2ef972941e7dc612a835cb
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-12-05 22:59:00 +0900

    feat : 과일 장수 - 최대 이익 구하기

---
+++ lv1/과일장수.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int k, int m, int[] score) {
+        int answer = 0;
+
+        int count = score.length; //총 사과 개수
+        int boxCount = count / m; //만들 수 있는 상자 개수
+        int remainder = count % m; //상자를 만들고 남는 사과 개수
+
+        Arrays.sort(score);
+
+        for (int i = remainder; i < count; i = i + m) {
+            int plus = score[i] * m;
+            answer = answer + plus;
+        }
+
+        return answer;
+    }
+}
