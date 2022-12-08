c81fb6db518b94c1331868c248e6eca6311f3b73
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-12-08 22:18:19 +0900

    refactor : 정규 표현식 방법추가

--- lv1/신규아이디추천.java
+++ lv1/신규아이디추천.java
@@ -7,11 +7,18 @@ class Solution {
         new_id = new_id.replaceAll("[^-_.a-z0-9]", "");
 
         // 3단계 new_id에서 마침표(.)가 2번 이상 연속된 부분을 하나의 마침표(.)로 치환합니다.
+        /* 정규식 표현은
+         * new_id = new_id.replaceAll("[^a-z0-9-_.]","");
+         * */
         while (new_id.contains("..")) {
             new_id = new_id.replace("..", ".");
         }
 
+
         // 4단계 new_id에서 마침표(.)가 처음이나 끝에 위치한다면 제거합니다.
+        /* 정규식 표현은
+         * new_id = new_id.replaceAll("^(\\.)|(\\.)$","");
+         * */
         if (new_id.length() > 0) {
             if (new_id.charAt(0) == '.') {
                 new_id = new_id.substring(1, new_id.length());
@@ -31,6 +38,11 @@ class Solution {
 
         // 6단계 new_id의 길이가 16자 이상이면, 첫 15개의 문자를 제외한 나머지 문자들을 모두 제거
         //      만약 제거 후 마침표(.)가 new_id의 끝에 위치한다면 끝에 위치한 마침표(.) 문자를 제거
+        /* 정규식 표현은
+         * if(new_id.length() >= 16) {
+         *      new_id = new_id.substring(0,15).replaceAll("(\\.)$", "");
+         * }
+         * */
         if (new_id.length() >= 16) {
             new_id = new_id.substring(0, 15);
         }
