928df38ec3fa368b28cd4026567a5ba7fae224b3
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-12-13 21:57:54 +0900

    feat : 가장 가까운 같은 글자 찾기

---
+++ lv1/가장가까운같은글자.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String s) {
+        int[] answer = new int[s.length()];
+        String[] clone = new String[s.length()];
+        String[] sArr = s.split("");
+
+        Map<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < sArr.length; i++) {
+            if (map.get(sArr[i]) == null) {
+                //없으면 -1
+                answer[i] = -1;
+                map.put(sArr[i], 0);
+                clone[i] = sArr[i];
+            } else {
+                //있던 것이면
+                int temp = Arrays.asList(clone).lastIndexOf(sArr[i]);
+
+                clone[i] = sArr[i];
+                answer[i] = i - temp;
+            }
+        }
+        return answer;
+    }
+}
