fe0be5365744d12384dbf256d0838287b4b66dda
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-12-23 17:58:57 +0900

    feat : 누적된 값이 최소가 되도록 만들기

---
+++ lv2/최솟값만들기.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int []A, int []B) {
+        int answer = 0;
+        Arrays.sort(A);
+        Arrays.sort(B);
+
+        // 한 쪽의 최댓값과 다른 쪽의 최솟값을 곱하는 경우 누적값을 최소로 구할 수 있다
+        for (int i = 0; i < A.length; i++) {
+            answer += A[i]*B[B.length-i-1];
+        }
+
+        return answer;
+    }
+}
