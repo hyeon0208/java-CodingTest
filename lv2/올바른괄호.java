b444fc6a2b1e71f448cd5dc309a9b4c4e26d9aee
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-12-25 23:06:39 +0900

    fix : 괄호가 바르게 짝지어졌는지 확인 (주석추가)

--- lv2/올바른괄호.java
+++ lv2/올바른괄호.java
@@ -9,18 +9,22 @@ class Solution {
         for(int i = 0; i < s.length(); i++){
             char ch = s.charAt(i);
 
+            // '(' 괄호로 시작한다면 스택에 '('를 채워넣는다.
             if(ch == '('){
                 stack.push(ch);
             }
 
+            // ')' 괄호로 시작하는데, 스택이 비었으면 '('로 시작하지 않으므로 false를 반환한다.
             if(ch == ')'){
-                if(stack.size() == 0) {
+                if(stack.isEmpty()) {
                     return false;
                 }
+                // 아니라면 '('와 ')'가 바르게 짝지어졌으므로 push 했던 '('괄호를 뺀다.
                 stack.pop();
             }
         }
 
+        // stack이 비었다면 괄호 쌍이 올바르게 짝지어졌으므로 true, 아니라면 false를 반환한다.
         answer = (stack.isEmpty()) ? true : false;
 
         return answer;
