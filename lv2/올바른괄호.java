5561e77c234e9bd55b7e3abf4ab3da82913c4e93
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-12-25 23:01:49 +0900

    feat : 괄호가 바르게 짝지어졌는지 확인

---
+++ lv2/올바른괄호.java
@@ -0,0 +1,28 @@
+import java.util.Stack;
+
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+
+        Stack<Character> stack = new Stack<>();
+
+        for(int i = 0; i < s.length(); i++){
+            char ch = s.charAt(i);
+
+            if(ch == '('){
+                stack.push(ch);
+            }
+
+            if(ch == ')'){
+                if(stack.size() == 0) {
+                    return false;
+                }
+                stack.pop();
+            }
+        }
+
+        answer = (stack.isEmpty()) ? true : false;
+
+        return answer;
+    }
+}
