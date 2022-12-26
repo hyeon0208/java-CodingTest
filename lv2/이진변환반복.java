332a7616327ac3d67f217aa4270d5542338b17dc
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-12-26 18:33:53 +0900

    feat : 이진 변환 반복하기

---
+++ lv2/이진변환반복.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] solution(String s) {
+        int[] answer = new int[2];
+
+        while (s.length() > 1) {
+            int one = 0;
+            for (int i = 0; i < s.length(); i++) {
+                if (s.charAt(i) == '0') {
+                    answer[1]++;
+                }
+                if (s.charAt(i) == '1') {
+                    one++;
+                }
+            }
+            s = Integer.toBinaryString(one);
+            answer[0]++;
+        }
+
+        return answer;
+    }
+}
