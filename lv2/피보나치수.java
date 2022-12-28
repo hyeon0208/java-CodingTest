a39e707467f765b3494d99c58f00965e9dafbea3
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-12-28 18:10:38 +0900

    feat : n번째 피보나치 수 구하기

---
+++ lv2/피보나치수.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+public class 피보나치수 {
+
+    public static void main(String[] args) {
+        System.out.println(solution(5));
+    }
+
+    public static int solution(int n) {
+        int[] answer = new int[n + 1];
+        answer[0] = 0;
+        answer[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            answer[i] = (answer[i - 1] + answer[i - 2]) % 1234567;
+        }
+
+        return answer[n];
+    }
+}
