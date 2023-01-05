2a1fa02088311182467c79e6fa943fadfeffe306
SuhHyeonjun <dmsgktn0208@gmail.com>
2023-01-05 19:39:29 +0900

    chore : 주석 추가

--- practice/date/DateFormatEx4.java
+++ practice/date/DateFormatEx4.java
@@ -10,26 +10,27 @@ class DateFormatEx4 {
     public static void main(String[] args) {
         String pattern = "yyyy/MM/dd";
         DateFormat df = new SimpleDateFormat(pattern);
-        Scanner s = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
 
-        Date inDate = null;
+        Date inputDate = null;
 
-        System.out.println("날짜를 " + pattern + "의 형태로 입력해주세요.(입력예:2015/12/31)");
-        while(s.hasNextLine()) {
+        System.out.println("날짜를 " + pattern + "의 형태로 입력해주세요.(입력예:2023/12/31)");
+        // hasNextLine()은 다음에 읽을 값이 있는지 확인한다.
+        while (input.hasNextLine()) {
             try {
-                inDate = df.parse(s.nextLine());
+                inputDate = df.parse(input.nextLine());
                 break;
-            } catch(Exception e) {
-                System.out.println("날짜를 " + pattern + "의 형태로 다시 입력해주세요.(입력예:2015/12/31)");
+            } catch (Exception e) {
+                System.out.println("날짜를 " + pattern + "의 형태로 다시 입력해주세요.(입력예:2023/12/31)");
             }
         }
 
         Calendar cal = Calendar.getInstance();
-        cal.setTime(inDate);
+        cal.setTime(inputDate);
 
         Calendar today = Calendar.getInstance();
-        long day = (cal.getTimeInMillis() - today.getTimeInMillis())/(60*60*1000);
+        long day = (cal.getTimeInMillis() - today.getTimeInMillis()) / (60 * 60 * 1000);
 
-        System.out.println("입력하신 날짜는 현재와 "+ day +"시간 차이가 있습니다.");
+        System.out.println("입력하신 날짜는 현재와 " + day + "시간 차이가 있습니다.");
     }
 }
