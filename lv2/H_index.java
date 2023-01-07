fb742463ded3c6f788faee20b99854a51735a345
SuhHyeonjun <dmsgktn0208@gmail.com>
2023-01-07 11:30:20 +0900

    feat : H-index 구하기

---
+++ lv2/H_index.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class H_index {
+    public static void main(String[] args) {
+        System.out.println(solution(new int[] {3, 0, 6, 1, 5}));
+    }
+
+    public static int solution(int[] citations) {
+        int answer = 0;
+        Arrays.sort(citations);
+
+        for (int i = 0; i < citations.length; i++) {
+            int h = citations.length - i;
+
+            if (citations[i] >= h) {
+                answer = h;
+                break;
+            }
+        }
+        return answer;
+    }
+}
