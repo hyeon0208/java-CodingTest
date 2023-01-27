7d17a61f471405773a3bf41e133f9288964132c3
SuhHyeonjun <dmsgktn0208@gmail.com>
2023-01-28 00:01:43 +0900

    feat : 카카오 [3차] 압축

---
+++ lv2/압축.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class 압축 {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(solution("KAKAO")));
+        System.out.println(Arrays.toString(solution("TOBEORNOTTOBEORTOBEORNOT")));
+
+    }
+
+    public static int[] solution(String msg) {
+        List<Integer> indexLists = new ArrayList<>();
+        Map<String, Integer> book = new HashMap<>();
+
+        char word = 'A';
+        for (int i = 1; i <= ('Z' - 'A') + 1; i++) {
+            book.put(word + "", i);
+            word += 1;
+        }
+
+        int idx = 0;
+        while(idx < msg.length()){
+            String w = "";
+            while(idx < msg.length()){
+                if(!book.containsKey(w+msg.charAt(idx))){
+                    break;
+                }else{
+                    w += msg.charAt(idx);
+                }
+                idx++;
+            }
+
+            indexLists.add(book.get(w));
+            if(idx < msg.length()){
+                book.put(w+msg.charAt(idx), book.size() + 1);
+            }
+        }
+        return indexLists.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
