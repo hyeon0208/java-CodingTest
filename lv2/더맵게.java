766bfc5ba914e65da4ee1e265dc102c9919ac892
SuhHyeonjun <dmsgktn0208@gmail.com>
2023-01-31 00:07:17 +0900

    feat : 더맵게 (우선순위 큐)

--- lv2/더맵게.java
+++ lv2/더맵게.java
@@ -3,8 +3,6 @@ import java.util.PriorityQueue;
 public class 더맵게 {
     public static void main(String[] args) {
         System.out.println(solution(new int[]{1, 2, 3, 9, 10, 12}, 7));
-        System.out.println(solution(new int[]{2, 3, 8, 9, 10, 12}, 8));
-        System.out.println(solution(new int[]{1}, 8));
     }
 
     public static int solution(int[] scoville, int K) {
