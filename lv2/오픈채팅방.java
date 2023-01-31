93d25ac8db6e78684a15672a08445ab97a137809
SuhHyeonjun <dmsgktn0208@gmail.com>
2023-01-31 23:51:01 +0900

    feat : 오픈채팅방 (HashMap 활용)

--- lv2/오픈채팅방.java
+++ lv2/오픈채팅방.java
@@ -3,7 +3,6 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.SimpleTimeZone;
 
 public class 오픈채팅방 {
     public static void main(String[] args) {
