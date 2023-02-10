977a60c66568ffac2335a19522f3442203024874
SuhHyeonjun <dmsgktn0208@gmail.com>
2023-02-11 00:01:22 +0900

    feat : 네트워크 연결 수 (dfs)

--- highscorekit/dfs_bfs/네트워크.java
+++ highscorekit/dfs_bfs/네트워크.java
@@ -5,7 +5,7 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class 네트워크 {
-    static boolean[] visited;
+    static boolean[] linked;
 
     public static void main(String[] args) {
         int[][] computers = {{1, 1, 0}, {1, 1, 0}, {0, 0, 1}};
@@ -14,10 +14,10 @@ public class 네트워크 {
 
     public static int solution(int n, int[][] computers) {
         int answer = 0;
-        visited = new boolean[n];
+        linked = new boolean[n];
 
         for (int i = 0; i < n; i++) {
-            if (!visited[i]) {
+            if (!linked[i]) {
                 answer++;
                 dfs(computers, i);
             }
@@ -27,12 +27,16 @@ public class 네트워크 {
     }
 
     private static void dfs(int[][] computers, int i) {
-        visited[i] = true;
+        if (linked[i]) {
+            return;
+        }
+
+        // computer[i][i] 자기자신은 항상 1
+        linked[i] = true;
 
         for (int j = 0; j < computers.length; j++) {
-            // computer[i][i] 자기자신은 항상 1
             // !visited[i] && computers[depth][i]은 자기자신이 아닌 것.
-            if (!visited[j] && computers[i][j] == 1) {
+            if (!linked[j] && computers[i][j] == 1) {
                 dfs(computers, j);
             }
         }
