451e1f3d2153e9ebf156e937b90bd1ccd9889ef9
SuhHyeonjun <dmsgktn0208@gmail.com>
2023-02-11 00:01:32 +0900

    feat : 네트워크 연결 수 (dfs)

--- lv2/뉴스클러스터링.java
+++ lv2/뉴스클러스터링.java
@@ -1,13 +1,16 @@
+import java.awt.BorderLayout;
 import java.awt.desktop.SystemSleepEvent;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class 뉴스클러스터링 {
     public static void main(String[] args) {
         System.out.println(solution("FRANCE", "french"));
+        System.out.println(solution("handshake", "shake hands"));
         System.out.println(solution("aa1+aa2", "AAAA12"));
-        System.out.println(solution("aaaa", "bbbb"));
         System.out.println(solution("E=M*C^2", "e=m*c^2"));
+        System.out.println(solution("abab", "baba"));
     }
 
     public static int solution(String str1, String str2) {
@@ -17,26 +20,33 @@ public class 뉴스클러스터링 {
 
         List<String> leftStr = splitStr(str1);
         List<String> rightStr = splitStr(str2);
-        List<String> union = new ArrayList<>();
-        List<String> intersection = new ArrayList<>();
+        List<String> union = new ArrayList<>(); // 합집합
+        List<String> intersection = new ArrayList<>(); // 교집합
 
-        System.out.println(leftStr);
-        System.out.println(rightStr);
+        System.out.println("left : " + leftStr);
+        System.out.println("right : " + rightStr);
+
+        Boolean[] visited = new Boolean[leftStr.size()];
 
         for (int i = 0; i < leftStr.size(); i++) {
-            if (rightStr.contains(leftStr.get(i))) {
+            visited[i] = false;
+            if (rightStr.contains(leftStr.get(i)) && !visited[leftStr.indexOf(leftStr.get(i))]) {
+                visited[leftStr.indexOf(leftStr.get(i))] = true;
                 union.add(leftStr.get(i));
-            } else {
+            }
+            if (!rightStr.contains(leftStr.get(i))) {
                 intersection.add(leftStr.get(i));
             }
         }
+        System.out.println(Arrays.toString(visited));
 
         for (String s : rightStr) {
             intersection.add(s);
         }
 
-        System.out.println(union);
-        System.out.println(intersection);
+
+        System.out.println("교집합 : " + union);
+        System.out.println("합집합 : " + intersection);
 
         double jakard = 0;
         double unionCount = union.size();
