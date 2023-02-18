c5ad704a45dbc485aa7e30aef7f1975931a3e972
SuhHyeonjun <dmsgktn0208@gmail.com>
2023-02-18 12:58:44 +0900

    feat : 2xn타일링 (DP 활용)

---
+++ lv2/타일링.java
@@ -0,0 +1,18 @@
+public class 타일링 {
+    public static void main(String[] args) {
+        System.out.println(solution(4));
+    }
+
+    public static int solution(int n) {
+        int[] arr = new int[n];
+        arr[0] = 1; // 세로
+        arr[1] = 2; // 가로
+
+        // arr[2] 부터 시작
+        for (int i = 2; i < arr.length; i++) {
+            arr[i] = arr[i - 1] + arr[i - 2] % 1000000007;
+        }
+
+        return arr[n - 1];
+    }
+}
